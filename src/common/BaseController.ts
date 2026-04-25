65001227f6715a944dc17412fd8547531303e3a1
henryf <fenghl@anmed.com.cn>
2026-04-26 07:34:26 +0800

    添加了controller，rel 0.5.0

--- src/common/BaseController.ts
+++ src/common/BaseController.ts
@@ -1,20 +1,15 @@
 
-import log4js, {Logger} from "log4js";
 import LoggedUser, {CommonUser} from "../LoggedUser";
 import {Request} from "express";
+import Controller from "./Controller";
 
 /**
  * Abstract base controller class providing common functionality for all controllers
  * @template T The service type this controller depends on
  */
-export default abstract class BaseController<T> {
-
-    /** Flag to enable debug logging */
-    static debugEnabled: boolean = false;
+export default abstract class BaseController<T> extends Controller {
     /** The service instance this controller uses */
     protected readonly service: T;
-    /** Logger instance for this controller */
-    protected readonly logger: Logger;
 
     /**
      * Constructor for base controller
@@ -22,19 +17,9 @@ export default abstract class BaseController<T> {
      * @protected
      */
     protected constructor(service: T) {
-        this.logger = log4js.getLogger(this.constructor.name);
+        super();
         this.service = service;
     }
 
-    /**
-     * Gets the current logged user, if acting as another user, returns the acted user,
-     * returns null for requests without user injection
-     * @param req Express request object
-     * @returns The current user or null if no user is logged in
-     */
-    protected getLoggedUser = (req: Request): CommonUser => {
-        let user: LoggedUser = req['user'];
-        return user?.actAs || user;
-    }
 
 }
---
+++ src/common/Controller.ts
@@ -0,0 +1,32 @@
+import log4js, {Logger} from "log4js";
+import {CommonUser, LoggedUser} from "../../lib";
+
+export default abstract class Controller {
+
+
+    /** Flag to enable debug logging */
+    static debugEnabled: boolean = false;
+
+    /** Logger instance for this controller */
+    protected readonly logger: Logger;
+
+    /**
+     * Constructor for base controller
+     * @protected
+     */
+    protected constructor() {
+        this.logger = log4js.getLogger(this.constructor.name);
+    }
+
+    /**
+     * Gets the current logged user, if acting as another user, returns the acted user,
+     * returns null for requests without user injection
+     * @param req Express request object
+     * @returns The current user or null if no user is logged in
+     */
+    protected getLoggedUser = (req: Request): CommonUser => {
+        let user: LoggedUser = req['user'];
+        return user?.actAs || user;
+    }
+
+}
